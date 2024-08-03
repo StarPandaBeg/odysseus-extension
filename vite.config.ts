23a9af1580889041ec126233736f5a06b133947d
StarPandaBeg <kshmurakov@mail.ru>
2024-08-03 14:56:08 +0300

    feat: use Roboto font

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import path from "path";
+import Unfonts from "unplugin-fonts/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), Unfonts({ google: { families: ["Roboto"] } })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
